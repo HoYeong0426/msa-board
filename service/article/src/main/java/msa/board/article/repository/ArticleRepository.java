f7879e4d7d2655f088462de8fcb700728ecbfdde
HEJ <ddo760@naver.com>
2025-09-30 18:25:37 +0900

    first commit

---
+++ service/article/src/main/java/msa/board/article/repository/ArticleRepository.java
@@ -0,0 +1,8 @@
+package msa.board.article.repository;
+
+import msa.board.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
